34e7080c32683d7866785b6d3ce850076cc98f25
mrnosomebody <73302906+mrnosomebody@users.noreply.github.com>
2022-05-20 14:42:40 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 import bot
-import dataclasses
-import requests
-from config import BEARER
 
-get_users_url = "https://api.twitter.com/2/users/by?usernames="
-# if __name__ == '__main__':
-#     bot.bot.infinity_polling()
-print(requests.get('https://api.twitter.com/2/users/by?usernames=elonmusk',
-                   headers={"Authorization": f"Bearer {BEARER}"})
-      .json()['data'])
+if __name__ == '__main__':
+    bot.bot.infinity_polling()
